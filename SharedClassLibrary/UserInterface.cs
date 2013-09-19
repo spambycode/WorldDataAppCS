c4d7379880878183b7b6e7de47b75ed2daa77daa
spambycode <spambyemail@gmail.com>
2013-09-18 22:25:25 -0400

    More changes

--- SharedClassLibrary/UserInterface.cs
+++ SharedClassLibrary/UserInterface.cs
@@ -12,18 +12,72 @@ namespace SharedClassLibrary
     public class UserInterface
     {
         //**************************** PRIVATE DECLARATIONS ************************
-
+        private StreamReader transDataFile;
+        private StreamWriter logFile;
 
         //**************************** PUBLIC GET/SET METHODS **********************
 
 
         //**************************** PUBLIC CONSTRUCTOR(S) ***********************
-        public UserInterface()
+        public UserInterface(bool OpenLog, bool OpenTrans)
         {
             Console.WriteLine("OK, UserInterface object created");
+
+            if(OpenLog)
+            {
+                logFile = new StreamWriter("Log.txt", true);
+            }
+
+            if(OpenTrans)
+            {
+                if(File.Exists("Transdata.txt"))
+                {
+                    transDataFile = new StreamReader("Transdata.txt");
+                }
+                else
+                {
+                    Console.WriteLine("Error no file called Transdata.txt");
+                }
+            }
+        }
+
+        public UserInterface()
+        {
+
         }
         //**************************** PUBLIC SERVICE METHODS **********************
 
+
+        //--------------------------------------------------------------------------
+        /// <summary>
+        /// Write to the log file
+        /// </summary>
+        /// <param name="input">string that wants to be inputted to the log file</param>
+        public void WriteToLog(string input)
+        {
+            if(logFile != null)
+            {
+                logFile.WriteLine(input);
+            }
+            else
+            {
+                Console.WriteLine("Log file is not opened");
+            }
+        }
+
+
+        public bool GetOneTransdata(out string query)
+        {
+            query = "";
+
+            if(transDataFile.EndOfStream == false)
+            {
+                query = transDataFile.ReadLine();
+
+                return false;
+            }
+            return true;
+        }
 
         //**************************** PRIVATE METHODS *****************************
 
--- UserApp/UserApp.cs
+++ UserApp/UserApp.cs
@@ -1,43 +1,4 @@
-﻿/* PROJECT: WorldDataAppCS (C#)         PROGRAM: UserApp
- * AUTHOR:
- * OOP CLASSES USED (for Asgn 1):  UserInterface, NameIndex
- *      PLUS FOR FUTURE ASGN:  DataStorage, CodeIndex
- * FILES ACCESSED: (only INDIRECTLY through the OOP classes)
- *      INPUT:   TransData*.txt         (handled by UserInterface class)
- *      OUTPUT:  Log.txt                (handled by UserInterface class)
- *      OUTPUT:  NameIndexBackup*.txt   (handled by NameIndex class)
- *      PLUS FOR FUTURE ASGN:
- *          OUTPUT: MainData*.bin (& "INPUT" to check for "empty locations")
- *                                      (handled by DataStorage class)
- *          OUTPUT: CodeIndexBackup*.bin (handled by CodeIndex class)
- *          AND NameIndexBackup*.bin will be a BINARY file, not TEXT
- *      where * is the appropriate fileNameSuffix.
- * DESCRIPTION:  The program itself is just the CONTROLLER which UTILIZES
- *      the SERVICES (public methods) of various OOP classes.
- *      It processes the transaction requests in TransData file, using
- *      NameIndex to facilitate efficient processing.  It sends the request
- *      and the result to the Log file.  [NameIndex will initially need to
- *      be loaded from the backup file back into the internal structure].
- *      creates an internal NameIndex from data in the RawData file,
- *      PLUS FOR FUTURE ASGN:
- *          The ACTUAL MainData will be provided to user (in Log file)
- *          rather than just the DataRecordPtr.  Also, transaction requests
- *          based on Id and Code are handled using CodeIndex and the random
- *          access MainData file.
- * CONTROLLER ALGORITHM:  Traditional sequential-stream processing - i.e., 
- *      loop til done with TransData
- *      {   input 1 transaction request from TransData
- *          switch to use that data to call appropriate service in NameIndex
- *                  class to handle request
- *      }
- *      finish up with TransData
- *      finish up with NameIndex
- *      PLUS FOR FUTURE ASGN: expanded range of services to handled additional
- *          Id and Code based requests
- *          (Similarly, finish up would need to. . .)
- * CAUTION:  The program code below DOES NOT DEAL DIRECTLY WITH
- *      TransData or NameIndex or Log.  Appropriate OOP classes handle those.  
- *******************************************************************************/
+﻿
 
 using System;
 
@@ -55,6 +16,33 @@ namespace UserApp
             //      or manually by developer & fix fileNameSuffix accordingly.
             // <WRITE APPROPRIATE CODE HERE>
             // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            int CommandCount = 0;
+            string command = "";
+            SharedClassLibrary.UserInterface UI = new UserInterface(true, true);
+            SharedClassLibrary.MainData MD = new MainData(false);
+
+            while(UI.GetOneTransdata(out command) == false)
+            {
+                switch(command.Substring(0, 2))
+                {
+                    case "QI":
+                        MD.QueryByID(command);
+                        break;
+                    case "LI":
+                        MD.ListById(command);
+                        break;
+                    case "IN":
+                        MD.InsertRecord(command);
+                        break;
+                    case "DI":
+                        MD.DeleteRecordByID(command);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
 
 
 
